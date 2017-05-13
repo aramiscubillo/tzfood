2988657d766f0abdaf7e81b244300b5c95904ac8
aramiscubillo <aramis.cubillo@gmail.com>
2017-05-12 20:09:03 -0600

    spring security basics

---
+++ tz-food-app/src/main/java/ts/tzfood/controller/LoginController.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package ts.tzfood.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author Aramis
+ *
+ */
+@Controller
+public class LoginController {
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(){
+	     return "login";
+	}
+	
+}
